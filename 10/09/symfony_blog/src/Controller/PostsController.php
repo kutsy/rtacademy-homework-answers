a0c6f2aa5d676f525b2e1dea688b5edc814e7f2e
RT <roman@romantelychko.com>
2021-11-12 05:52:01 +0200

    10/09

--- 10/09/symfony_blog/src/Controller/PostsController.php
+++ 10/09/symfony_blog/src/Controller/PostsController.php
@@ -23,19 +23,50 @@ class PostsController extends AbstractController
             [
                 'top_post'          => $top_post,
                 'posts'             => $latest_posts_paginator,
+                'next_page_offset'  => min( count( $latest_posts_paginator ), PostRepository::PAGINATOR_PER_PAGE ),
+            ]
+        );
+    }
+
+    #[Route('/post/list/{offset}', name: 'posts_list', methods: ['GET', 'HEAD'])]
+    public function list( int $offset, PostRepository $postRepository ): Response
+    {
+        // Останні N активних записів зі зміщенням $offset від початку
+        $posts_paginator = $postRepository->getLatestPosts( $offset );
+
+        return $this->render(
+            'posts/list.html.twig',
+            [
+                'posts'                 => $posts_paginator,
+                'previous_page_offset'  => $offset - PostRepository::PAGINATOR_PER_PAGE,
+                'next_page_offset'      => min( count( $posts_paginator ), $offset + PostRepository::PAGINATOR_PER_PAGE ),
             ]
         );
     }
 
     #[Route('/post/{id}-{alias}', name: 'post_view', methods: ['GET', 'HEAD'])]
-    public function view( int $id, PostRepository $postRepository ): Response
+    public function view( int $id, string $alias, PostRepository $postRepository ): Response
     {
         // Отримання активного запису за ID
         $post = $postRepository->getActivePost( $id );
 
         if( !$post )
         {
+            // запис не існує - 404
             throw $this->createNotFoundException( 'Post with #' . $id . ' not found' );
+        }
+
+        if( $post->getAlias() !== $alias )
+        {
+            // на випадок некоректного alias у URI -> переходимо до коректної сторінки
+            return $this->redirectToRoute(
+                'post_view',
+                [
+                    'id'    => $post->getId(),
+                    'alias' => $post->getAlias(),
+                ],
+                301
+            );
         }
 
         return $this->render(
